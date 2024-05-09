6523b77ddce2d7ba7319aea4f9336bcf502546ba
Song <songsongha@gmail.com>
2024-05-08 22:17:46 -0600

    Add tabulation method to traveler problem

--- codeCamp/traveler.ts
+++ codeCamp/traveler.ts
@@ -31,3 +31,27 @@ const gridTraveler2 = (m: number, n: number, memo:TravelMemo = {}): number =>{
 }
 
 console.log(gridTraveler2(18,18)) // 2333606220
+
+// tabulation method,
+
+// take your current position and add it to your neighbors
+// complexity is driven by size of table
+// time: O(mn)
+// space: O(nm)
+/* */
+const gridTravelerTabulation = (m: number, n: number) => {
+    const table = Array(m + 1).fill([]).map(() => Array(n + 1).fill(0))
+    table[1][1] = 1
+
+    for (let i = 0; i <= m; i++){
+        for(let j = 0; j <= n; j++){
+            const current = table[i][j]
+            if (j + 1 <= n) table[i][j+1] += current
+            if (i + 1 <= m) table[i+1][j] += current
+        }
+    }
+    return table[m][n]
+}
+
+console.log(gridTravelerTabulation(3,3) )// 6
+console.log(gridTravelerTabulation(18,18)) // 2333606220
