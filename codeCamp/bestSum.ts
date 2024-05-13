1d369afcab347ca66eb45e40d61e3978e9832a84
Song <songsongha@gmail.com>
2024-05-13 10:50:53 -0600

    Add tabulation method to bestSum

--- codeCamp/bestSum.ts
+++ codeCamp/bestSum.ts
@@ -39,3 +39,26 @@ console.log(bestSum(7,[5,3,4,7])) // [7]
 console.log(bestSum(8,[2,3,5])) // [3,5]
 console.log(bestSum(8, [1,4,5])) // [4,4]
 console.log(bestSum(100, [1,2,5,25])) // [25,25,25,25]
+
+// Tabulation method
+// time complexity O(n*m^2)
+// space complexity O(m^2)
+const bestSumTabulation = (targetSum: number, numArray: number[]): number[] | null => {
+    const table = new Array(targetSum + 1).fill(null)
+    table[0] = []
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] !== null) {
+            for (let num of numArray) {
+                if (!table[i+num] || table[i+num].length > table[i].length + 1)
+                table[i + num] = [...table[i], num]
+            }
+        }
+    }
+    
+    return table[targetSum]
+}
+
+console.log(bestSumTabulation(8, [2,3,5])) // [3,5]
+console.log(bestSumTabulation(7, [5,3,4,7])) // [7]
+console.log(bestSumTabulation(8, [1,4,5])) // [4,4]
+console.log(bestSumTabulation(100, [1,2,5,25])) // [25,25,25,25]
