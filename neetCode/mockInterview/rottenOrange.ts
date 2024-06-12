c9fe585fa86ba0185387756756b875cf073a3d31
Song <songsongha@gmail.com>
2024-06-12 08:54:33 -0600

    Add minutes tracking for rottenOrange

--- neetCode/mockInterview/rottenOrange.ts
+++ neetCode/mockInterview/rottenOrange.ts
@@ -2,7 +2,7 @@
 
  function orangesRotting(grid: number[][]): number {
         type Location = [number, number]
-        const rottenOrangeList: Location[] = []
+        const rottenOrangeList: (Location| null)[] = []
 
         // get location of rotting oranges
         for (let rowIndex = 0; rowIndex < grid.length; rowIndex ++){
@@ -14,27 +14,52 @@
             }
         }
 
+        let minutes = -1
+        rottenOrangeList.push(null)
         while (rottenOrangeList.length > 0) {
             // remove the element from the rottenOrangeList
             const location = rottenOrangeList.shift()
-            if (!location) break  
+            if (!location){
+                minutes++
+                if (rottenOrangeList.length > 0) rottenOrangeList.push(null)
+                continue
+            } 
             const [row, col] = location
-            // check for out of bounds
-            if (row > grid.length || row < 0 || col > grid[0].length) continue
+            console.log({location})
 
-            const adjacent = [-1, 1]
-            adjacent.forEach(element =>{
-                const value1 = grid[row + element][col]
-                const value2 = grid[row][col + element]
-                if (value1 === 1){
-                    grid[row + element][col] = 2
-                    rottenOrangeList.push([row + element, col])
+            if (row + 1 < grid.length){
+                const below = grid[row + 1][col]
+                if (below === 1){
+                    grid[row + 1][col] = 2
+                    rottenOrangeList.push([row + 1, col])
                 }
-                if (value2 === 1){
-                    grid[row][col + element] = 2
-                    rottenOrangeList.push([row, col + element])
+
+            }
+            if (row - 1 >= 0){
+                console.log("row - 1 >= 0",row - 1 >= 0)
+                const above = grid[row - 1][col]
+                if (above === 1){
+                    grid[row - 1][col] = 2
+                    rottenOrangeList.push([row - 1, col])
                 }
-            })     
+
+            }
+            if (col + 1 < grid[0].length){
+                const right = grid[row][col+1]
+                if (right === 1){
+                    grid[row][col+1] = 2
+                    rottenOrangeList.push([row, col+1])
+                }
+
+            }
+            if (col - 1 >= 0){
+                const left = grid[row][col-1]
+                if (left === 1){
+                    grid[row][col-1] = 2
+                    rottenOrangeList.push([row, col-1])
+                }
+
+            }
         }
         // check if the grid still contains fresh oranges
         for (let rowIndex = 0; rowIndex < grid.length; rowIndex ++){
@@ -43,5 +68,8 @@
             }
         }
         
-    return -1
+    return minutes
 }
+
+console.log(orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))
+console.log(orangesRotting([[0,2]]))
